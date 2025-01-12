c9993ee62b69295a108d406cd57944259572367a
KTajerbashi <kamrantajerbashi@gmail.com>
2025-01-13 01:55:58 +0330

    Feature : Add some changes

--- app/pages/about/about.controller.js
+++ app/pages/about/about.controller.js
@@ -1,4 +1,8 @@
 app.controller("AboutController", function ($scope) {
   console.log("AboutController Loaded");
-  $scope.message = "Welcome to the About Page!";
+  $scope.title = "Welcome to the About Page!";
+  $scope.content = "Content";
+  $scope.ChangeContent = (value) => {
+    $scope.content = value;
+  };
 });
