1e6e04876bde18ace60baabf3739c969598b98e3
KTajerbashi <kamrantajerbashi@gmail.com>
2025-01-17 20:07:02 +0330

    Feature : Profile Completed

--- app/pages/profile/profile.controller.js
+++ app/pages/profile/profile.controller.js
@@ -2,5 +2,18 @@ app.controller("ProfileController", [
   "$scope",
   function ($scope) {
     $scope.message = "This is the Calendar Page!";
+    $scope.emptyProfileViewModel = {
+      email: "kamrantajerbashi@gmail.com",
+      name: "kamran",
+      family: "Tajebashi",
+      phoneNumber: "09020320844",
+      username: "Tajerbashi",
+      password: "@Tajerbashi!123",
+      country: "2",
+      isRemember: true,
+      image: "../../../assets/images/img3.jpg",
+    };
+    $scope.profileViewModel = angular.copy($scope.emptyProfileViewModel);
+    console.log("$scope.profileViewModel : ", $scope.profileViewModel);
   },
 ]);
