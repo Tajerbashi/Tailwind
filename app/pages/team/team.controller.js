e80029b1eeabc09d66f0ae4209ec6005cbbf94d6
KTajerbashi <kamrantajerbashi@gmail.com>
2025-01-18 21:44:24 +0330

    Feature : Add Team Pages

--- app/pages/team/team.controller.js
+++ app/pages/team/team.controller.js
@@ -1,6 +1,73 @@
 app.controller("TeamController", [
   "$scope",
   function ($scope) {
-    $scope.message = "This is the Calendar Page!";
+    $scope.coleags = [
+      {
+        id: 1,
+        title: "Tajerbahsi",
+        description: "Backend Web Developer",
+        img: "../../../assets/images/img1.jpg",
+      },
+      {
+        id: 2,
+        title: "Mirzaie",
+        description: "Angula  Developer",
+        img: "../../../assets/images/img2.jpg",
+      },
+      {
+        id: 3,
+        title: "Kosari",
+        description: "Typescript Stack Web Developer",
+        img: "../../../assets/images/img3.jpg",
+      },
+      {
+        id: 4,
+        title: "Rezaiee",
+        description: "C# Web Developer",
+        img: "../../../assets/images/img4.jpg",
+      },
+      {
+        id: 5,
+        title: "Jason",
+        description: "Javascript Stack Web Developer",
+        img: "../../../assets/images/img5.jpg",
+      },
+      {
+        id: 6,
+        title: "Taylor",
+        description: "Architecture Web Developer",
+        img: "../../../assets/images/img6.jpg",
+      },
+      {
+        id: 7,
+        title: "Taylor",
+        description: "Architecture Web Developer",
+        img: "../../../assets/images/img7.jpg",
+      },
+      {
+        id: 8,
+        title: "Taylor",
+        description: "Architecture Web Developer",
+        img: "../../../assets/images/img8.jpg",
+      },
+      {
+        id: 9,
+        title: "Taylor",
+        description: "Architecture Web Developer",
+        img: "../../../assets/images/img9.jpg",
+      },
+      {
+        id: 10,
+        title: "Taylor",
+        description: "Architecture Web Developer",
+        img: "../../../assets/images/img10.jpg",
+      },
+      {
+        id: 11,
+        title: "Taylor",
+        description: "Architecture Web Developer",
+        img: "../../../assets/images/img1.jpg",
+      },
+    ];
   },
 ]);
