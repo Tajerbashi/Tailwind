67f53fe7115555d44329297d16b902f189f73282
KTajerbashi <kamrantajerbashi@gmail.com>
2025-01-26 23:44:04 +0330

    feature : Add Grid Page

--- app/pages/grid/grid.controller.js
+++ app/pages/grid/grid.controller.js
@@ -1,1 +1,26 @@
-app.controller("GridController", function ($scope) {});
+app.controller("GridController", function ($scope) {
+  const toggleVisible = (item) => {
+    item.visible = !item.visible;
+    console.log("Item : ", item);
+  };
+  const basePath = "/app/pages/grid/templates/";
+  $scope.buttons = [
+    {
+      title: "Controller",
+      action: (item) => {
+        toggleVisible(item);
+      },
+      visible: false,
+      path: basePath + "controller.html",
+    },
+
+    {
+      title: "Grid",
+      action: (item) => {
+        toggleVisible(item);
+      },
+      visible: true,
+      path: basePath + "gridContainer.html",
+    },
+  ];
+});
